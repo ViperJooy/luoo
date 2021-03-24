9ddc63fd2d8110eb9ea385b7d74101b6365dbc5b
viperjooy <viperjooy@gmail.com>
2021-03-24 17:40:00 +0800

    change api vercel

--- js/index.js
+++ js/index.js
@@ -26,7 +26,7 @@ $(function() {
         songId,
         songUrl,
         // baseURL = "/music",
-        baseURL = "http://music.viper.work/music",
+        baseURL = "https://netease-cloud-music-api-three-tau.vercel.app/",
         currIndex = -1;
 
     function playPause() {
